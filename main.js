085e876564f904b83572e30bfc4d4db03a2d78e7
fedesidero <fsidero2005@gmail.com>
2023-02-16 05:49:13 -0300

    actualizacion decimales

--- main.js
+++ main.js
@@ -1,27 +1,32 @@
 const display = document.querySelector("#display");
 const buttons = document.querySelectorAll("button");
-const result = document.querySelector("#result")
-
+const resultdisplay = document.querySelector("#result")
+var result
+var result2
 buttons.forEach((item) => {
     item.onclick = () => {
         if (item.id == "clear"){
             display.innerText = "";
-            result.innerText = "";
+            resultdisplay.innerText = "";
         } else if (item.id == "backspace"){
             let string = display.innerText.toString();
             display.innerText = string.substr(0, string.length - 1);
-            result.innerText = string.substr(0, string.length - 1);
+            resultdisplay.innerText = string.substr(0, string.length - 1);
         } else if (display.innerText != "" && item.id == "equal"){
-            display.innerText = eval(display.innerText)
-            result.innerText = "";
+            result = eval(display.innerText)
+            result2 = Number((result).toFixed(10))
+            display.innerText = result2
+            resultdisplay.innerText = "";
         } else if (display.innerText == "" && item.id == "equal"){
             display.innerText = "Empty"
             setTimeout(() => (display.innerText = ""), 2000);
         } else{
-            display.innerText += item.innerHTML;
+            display.innerText += item.id;
         }
         if (display.innerText != "" && item.id != "equal"){
-            result.innerText = eval(display.innerText);
+            result = eval(display.innerText)
+            result2 = Number((result).toFixed(2))
+            resultdisplay.innerText = result2
         }
     }
 })
@@ -29,10 +34,12 @@ buttons.forEach((item) => {
 const themeToggleBtn = document.querySelector(".theme-toggler");
 const calculator = document.querySelector(".calculator");
 const toggleIcon = document.querySelector(".toggler-icon");
+const bg = document.querySelector(".body");
 let isDark = true;
 themeToggleBtn.onclick = () => {
     calculator.classList.toggle("dark");
     themeToggleBtn.classList.toggle("active")
+    bg.classList.toggle("dark")
     isDark = !isDark
 }
 
